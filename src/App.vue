f2d14455d4e73db66fb90f7a79dd0665464a4ac8
Edgar <edgaarr2005@hotmail.com>
2025-03-27 15:43:14 -0400

    BG Change and binding user Name to form.

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import TheForm from './components/TheForm.vue';
 export default {
   components: {
     TheForm
-  }  
+  }
 }
 </script>
 
@@ -22,7 +22,8 @@ html {
 }
 
 body {
+  height: 100vh;
   margin: 0;
-  background-color: #292929;
+  background: linear-gradient(180deg, rgba(14, 0, 255, 1) 0%, rgba(0, 0, 105, 1) 50%, rgba(0, 214, 255, 1) 100%);
 }
 </style>
